3fc5d3cb06bec84956b9a2551cb20485fe74c612
Tomasz Wolak <wolak.t.23@gmail.com>
2022-02-27 10:20:29 +0100

    clear

--- src/device/Car.java
+++
@@ -1,9 +0,0 @@
-package device;
-
-public class Car extends Device {
-
-    @Override
-    void turnOn() {
-        System.out.println("Funkcja turnOn() Car");
-    }
-}
--- src/device/Phone.java
+++
@@ -1,8 +0,0 @@
-package device;
-
-public class Phone extends Device {
-    @Override
-    void turnOn() {
-        System.out.println("Funkcja turnOn() Phone");
-    }
-}
